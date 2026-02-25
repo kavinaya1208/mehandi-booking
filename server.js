5d49084f41955cb47bef771a1b5c768e10670380
kavinaya1208 <Kavinayakathavarayan12@gmail.com>
2026-02-25 17:52:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,9 +18,7 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/art.html");
 });
 
-require("dotenv").config();
 
-const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
@@ -123,6 +121,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
 
 
